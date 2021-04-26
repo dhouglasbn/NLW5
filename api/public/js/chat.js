50037d27da91c7a678786ccbaabd83c805f951e5
Dhouglas Bandeira <62654697+dhouglasbn@users.noreply.github.com>
2021-04-26 11:29:45 -0300

    commenting my code

--- api/public/js/chat.js
+++ api/public/js/chat.js
@@ -1,35 +1,45 @@
+
+// variáveis que serão mutadas durante o código
 let socket_admin_id = null;
 let emailUser = null;
 let socket = null
 
+// escutando em tempo real o botão #start_chat, quando for clicado :
 document.querySelector("#start_chat").addEventListener("click", (event) => {
     
     
-
+    // tirar a box chat_help da tela
     const chat_help = document.getElementById("chat_help");
     chat_help.style.display = "none";
 
+    // por a chat_in_support na tela
     const chat_in_support = document.getElementById("chat_in_support");
     chat_in_support.style.display = "block";
 
+    // atribuindo a socket as ferramentas para trabalhar com socket.io
     socket = io();
     
 
+    // pegando o valor do email inserido pelo user
     const email = document.getElementById("email").value;
     emailUser = email;
 
+    // pegando o valor do texto inserido pelo user
     const text = document.getElementById("txt_help").value;
 
+    // quando haver uma conexão socket
     socket.on("connect", () => {
         const params = {
             email,
             text
         }
+        // emitir "client_first_access" com params, call e err
         socket.emit("client_first_access", params, (call, err) => {
+            // mostrar no console o erro
             if  (err) {
 
                 console.err(err);
-            } else {
+            } else { // mostrar no console o callback
                 console.log(call)
             }
         })
@@ -63,7 +73,9 @@ document.querySelector("#start_chat").addEventListener("click", (event) => {
         }) 
     })
 
+    // quando for emitida "admin_send_to_client":
     socket.on("admin_send_to_client", message => {
+        // atribuir a socket_admin_id a socket.id da mensagem
         socket_admin_id = message.socket_id;
 
         // pegando o conteúdo da div admin-template
@@ -107,6 +119,7 @@ document.querySelector("#send_message_button").addEventListener("click", event =
     // adicionando rendered a messages
     document.getElementById("messages").innerHTML += rendered;
 
+    // apagando o valor da text
     text.value = "";
 })
 
